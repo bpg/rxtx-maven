7840be5f3f600c8caa3b25cd5c233a1128635873
Pavel Boldyrev <pboldyrev@gmail.com>
2012-11-24 21:25:46 -0500

    added installation of native library (.so)

--- install.sh
+++ install.sh
@@ -13,8 +13,10 @@ wget -q http://rxtx.qbang.org/pub/rxtx/$NAME-bins.zip > /dev/null
 echo Preparing
 unzip $NAME-bins.zip > /dev/null
 
-echo Installing mavern artifact
+echo Installing Maven artifact: jar
 mvn install:install-file -Dfile=$NAME-bins/RXTXcomm.jar -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dpackaging=jar -DgeneratePom=true > /dev/null
+echo Installing Maven artifact: bin
+mvn install:install-file -Dfile=$NAME-bins/x86_64-unknown-linux-gnu/librxtxSerial.so -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dpackaging=so -Dclassifier=bin > /dev/null
 
 echo Downloading sources of $NAME
 wget -q http://rxtx.qbang.org/pub/rxtx/$NAME.zip
@@ -25,7 +27,7 @@ cd $NAME/src
 find . -name "CVS" -type d -print | xargs rm -rf
 jar cf src.jar ./gnu
 
-echo Installing mavern artifact
+echo Installing Maven artifact: sources
 mvn install:install-file -Dfile=src.jar -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dpackaging=jar -Dclassifier=sources > /dev/null
 
 cd ../../
